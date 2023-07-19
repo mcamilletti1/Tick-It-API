de256602dfeefddda4c4cb1842455d4727d6e9fa
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-19 16:07:18 -0400

    set up search endpoints for type and search for events by venue

--- tickit_project/tickit/urls.py
+++ tickit_project/tickit/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('events', views.EventList.as_view(), name='event_list'),
     path('venues/<int:pk>', views.VenueDetail.as_view(), name='venue_detail'),
     path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
-    path('venues', views.VenueList.as_view(), name='venue_list')
+    path('venues', views.VenueList.as_view(), name='venue_list'),
+    path('venues/<int:pk>/events/', views.VenueEventsList.as_view(), name='venue-events-list'),
+    path('events/search/type/<str:event_type>/', views.EventTypeSearch.as_view(), name='event-type-search')
 ]
