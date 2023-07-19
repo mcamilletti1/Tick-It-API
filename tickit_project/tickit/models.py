97ef17c198ef6950d9f14eb7c1ef5125d0813f16
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-19 13:36:56 -0400

    updated models and seeded data

--- tickit_project/tickit/models.py
+++ tickit_project/tickit/models.py
@@ -6,6 +6,7 @@ class Venue(models.Model):
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+    photo_url = models.TextField(default="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")
 
     def __str__(self):
         return self.name
@@ -13,12 +14,13 @@ class Venue(models.Model):
 
 class Event(models.Model):
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='events')
-    artist = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     date = models.CharField(max_length=100)
     time = models.CharField(max_length=100)
-    type = models.CharField(max_length=100, default="Live Music")
+    type = models.CharField(max_length=100, default="Event")
+    photo_url = models.TextField(default="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")
 
     def __str__(self):
-        return self.artist
+        return self.name
     
 
